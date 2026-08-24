9ff5d662f775ef6847462125cc33bff7dc68927c
313danish <mdd48266@gmail.com>
2026-08-24 09:42:22 +0530

    added backend

---
+++ Node.js/Node1.js
@@ -0,0 +1,7 @@
+function Sum(a , b ){
+    return a + b;
+}
+
+// console.log(Sum(5, 10));
+
+module.exports = Sum;
---
+++ Node.js/Node2.js
@@ -0,0 +1,10 @@
+// function sum(a , b ){
+//     return a + b;
+// }   
+
+// console.log(sum(10, 10));
+
+
+
+const sum = require('./Node1');
+console.log(sum(10, 10));
