c8a7507cc7185e86ec55321281a44192051fea1d
313danish <mdd48266@gmail.com>
2026-08-24 11:53:09 +0530

    added node folder

--- Node.js/Node1.js
+++ Node.js/Node1.js
@@ -1,7 +1,12 @@
-function Sum(a , b ){
+console.log("Hello, this is Node.js code running in the Amazon Clone App!");
+
+
+function sum(a, b) {
     return a + b;
-}
+}   
 
-// console.log(Sum(5, 10));
 
-module.exports = Sum;
+// console.log("The sum of 5 and 10 is: " + sum(5, 10));
+
+
+module.exports = sum
--- Node.js/Node2.js
+++ Node.js/Node2.js
@@ -1,10 +1,12 @@
-// function sum(a , b ){
+// function sum(a, b) {
 //     return a + b;
 // }   
 
-// console.log(sum(10, 10));
+// console.log("The sum of 5 and 10 is: " + sum(5, 10));
 
 
 
-const sum = require('./Node1');
-console.log(sum(10, 10));
+
+const x = require('./Node1.js')
+
+console.log(x(5, 10))
