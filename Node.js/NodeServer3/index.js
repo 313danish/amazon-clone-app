5eb99f017d8b799334e42819509c6249eff61f0e
313danish <mdd48266@gmail.com>
2026-09-18 09:43:49 +0530

    added mongo db notes as ppt

--- Node.js/NodeServer3/index.js
+++ Node.js/NodeServer3/index.js
@@ -20,16 +20,66 @@ app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/danish.html');
 // });
 
+const users = {
+  sonu: {
+    username: 'sonu',
+    firstName: 'Sonu',
+    profileImage: 'https://reqres.in/img/faces/1-image.jpg',
+    email: 'sonu@example.com',
+    premium: true,
+    hobbies: ['Reading', 'Traveling', 'Cooking'],
+  },
+  danish: {
+    username: 'danish',
+    firstName: 'Danish',
+    profileImage: 'https://reqres.in/img/faces/1-image.jpg',
+    email: 'danish@example.com',
+    premium: false,
+    hobbies: ['Painting', 'Gardening', 'Yoga'],
+  },
+  deeksha: {
+    username: 'deeksha',
+    firstName: 'Deeksha',
+    profileImage: 'https://reqres.in/img/faces/2-image.jpg',
+    email: 'deeksha@example.com',
+    premium: true,
+    hobbies: ['Painting', 'Gardening', 'Yoga'],
+  }
+};
 
-app.get('/danish', (req, res) => {
-  res.render('profile', { firstName: 'Danish', profileImage: 'https://reqres.in/img/faces/1-image.jpg', email: 'danish@example.com' }); // Render the EJS template for sonu with data
+// app.get('/danish', (req, res) => {
+//   res.render('profile', { firstName: 'Danish', profileImage: 'https://reqres.in/img/faces/1-image.jpg', email: 'danish@example.com' }); // Render the EJS template for sonu with data
+// });
+
+app.get('/:user', (req, res) => {
+  // console.log(req.params.user);
+  const userdata = users[req.params.user];
+  if (userdata) {
+    res.render('profile', userdata);
+  } else {
+    // res.status(404).send('User not found');
+    res.status(404).sendFile(__dirname + '/views/404.html');  // to send html file for 404 error
+  }
 });
-app.get('/poorvi', (req, res) => {
-  res.render('profile', { firstName: 'Poorvi', profileImage: 'https://reqres.in/img/faces/3-image.jpg', email: 'poorvi@example.com' }); // Render the EJS template for sonu with data
-});
-app.get('/deeksha', (req, res) => {
-  res.render('profile', { firstName: 'Deeksha' , profileImage: 'https://reqres.in/img/faces/2-image.jpg', email: 'deeksha@example.com' }); // Render the EJS template for sonu with data
-});
+
+// app.get('/poorvi', (req, res) => {
+//   res.render('profile', { 
+//     firstName: 'Poorvi', 
+//     profileImage: 'https://reqres.in/img/faces/3-image.jpg', 
+//     email: 'poorvi@example.com',
+//     premium : false,
+//     hobbies: ['Painting', 'Gardening', 'Yoga'],
+//   }); // Render the EJS template for sonu with data
+// });
+// app.get('/deeksha', (req, res) => {
+//   res.render('profile', { 
+//     firstName: 'Deeksha' , 
+//     profileImage: 'https://reqres.in/img/faces/2-image.jpg', 
+//     email: 'deeksha@example.com',
+//     premium : true, 
+//     hobbies: ['Reading', 'Traveling', 'Cooking'],
+//   }); // Render the EJS template for sonu with data
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000 http://localhost:3000');
--- Node.js/Nodeserver4/index.js
+++ Node.js/Nodeserver4/index.js
@@ -1,14 +1,97 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+// app.get('/sonu', (req, res) => {
+//   res.sendFile(__dirname + '/sonu.html');
+// }   );
+
+// app.get('/danish', (req, res) => {
+//   res.sendFile(__dirname + '/danish.html');
+// });
+
+
+const users = {
+  sonu: {
+    firstName: 'Sonu',
+    profileImage: 'https://reqres.in/img/faces/1-image.jpg',
+    email: 'sonu@example.com',
+    premium: true,
+    hobbies: ['Reading', 'Traveling', 'Cooking'],
+  },
+  danish: {
+    firstName: 'Danish',
+    profileImage: 'https://reqres.in/img/faces/2-image.jpg',
+    email: 'danish@example.com',
+    premium: false,
+    hobbies: ['Painting', 'Gardening', 'Yoga'],
+  },
+  pruthvi: {
+    firstName: 'Pruthvi',
+    profileImage: 'https://reqres.in/img/faces/3-image.jpg',
+    email: 'pruthvi@example.com',
+    premium: true,
+    hobbies: ['Photography', 'Cycling', 'Cooking'],
+  },
+};
+
+
+app.get('/:user', (req, res) => {
+  const userdata = users[req.params.user];
+  console.log(req.params.user);
+  if (userdata) {
+    res.render('profile', userdata);
+  } else {
+    // res.status(404).send('User not found');
+    res.status(404).sendFile(__dirname + '/views/404.html');
+  }
+});
+
+
+
+// app.get('/sonu', (req, res) => {
+//   res.render('profile', users.sonu);
+// });
+
+// app.get('/danish', (req, res) => {
+//   res.render('profile', { firstName: 'Danish', profileImage: 'https://reqres.in/img/faces/1-image.jpg', email: 'danish@example.com' }); // Render the EJS template for sonu with data
+// });
+// app.get('/poorvi', (req, res) => {
+//   res.render('profile', { 
+//     firstName: 'Poorvi', 
+//     profileImage: 'https://reqres.in/img/faces/3-image.jpg', 
+//     email: 'poorvi@example.com',
+//     premium : false,
+//     hobbies: ['Painting', 'Gardening', 'Yoga'],
+//   }); // Render the EJS template for sonu with data
+// });
+
+// app.get('/deeksha', (req, res) => {
+//   res.render('profile', { 
+//     firstName: 'Deeksha' , 
+//     profileImage: 'https://reqres.in/img/faces/2-image.jpg', 
+//     email: 'deeksha@example.com',
+//     premium : true, 
+//     hobbies: ['Reading', 'Traveling', 'Cooking'],
+//   }); // Render the EJS template for sonu with data
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000 http://localhost:3000');
-}   );
+});
 
 
-// run server using command: node index.js
+
+// /* view engine setup (dynamic pages) 
+// Request parameters (dynamic urls/routes)
+// */
