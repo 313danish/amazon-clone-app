2418c519126d3fb6ee04898c39835661567a8d4b
313danish <mdd48266@gmail.com>
2026-09-23 09:51:17 +0530

    added schema and notes to git

--- Node.js/NodeServer5/index.js
+++ Node.js/NodeServer5/index.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ejs = require('ejs');
+// const mongoose = require('mongoose');
 
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs'); // Set EJS as the view engine    
-mongoose.
-    connect('mongodb+srv://admin:admin%40123@cluster0.p1jxd0c.mongodb.net/?appName=Cluster0').
-    then(() => {
-        console.log('Connected to MongoDB');
-    }).catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
-    
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
-});
+// app.set('view engine', 'ejs'); // Set EJS as the view engine    
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000 at http://localhost:3000');
-});
+
+// mongoose.
+//     connect('mongodb+srv://admin:admin%40123@cluster0.p1jxd0c.mongodb.net/?appName=Cluster0').
+//     then(() => {
+//         console.log('Connected to MongoDB');
+//     }).catch((err) => {
+//         console.error('Error connecting to MongoDB:', err);
+//     });
+
+
+// app.get('/', (req, res) => {
+//     res.json({ message: 'Hello World' });
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000 at http://localhost:3000');
+// });
+
 
 // run
 
@@ -53,3 +57,46 @@ app.listen(3000, () => {
 //     - lastName: String
 //     - email: String
 //     - phoneNumber: Number
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+require('dotenv').config()
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs'); // Set EJS as the view engine    
+
+
+mongoose.
+    connect(process.env.MONGODB_URL).
+    then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+const User= mongoose.model('User', {
+    firstName:String,
+    lastName:String,
+    email:String,
+    Phone:Number,
+});
+const Child= mongoose.model('Child', {
+    firstName:String,
+    lastName:String,
+    email:String,
+    Phone:Number,
+});
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}  at http://localhost:${PORT}`);
+});
